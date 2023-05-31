fdba67e0ebbfeda6a93c1af19484993815b0038b
Christian <chcarov@correo.udistrital.edu.co>
2023-05-30 19:12:03 -0500

    Trigger para verificación de las tarifas de las sucursales implementado.

--- PL-pgSQL/Triggers.sql
+++ PL-pgSQL/Triggers.sql
@@ -99,6 +99,8 @@ BEGIN
     ELSIF NEW.K_EMPLEADO IS NOT NULL AND NEW.K_CLIENTE IS NOT NULL THEN
         RAISE EXCEPTION 'Se está tratano de auditar a un cliente y un empleado a la vez.';
     END IF;
+    -- Continúa con la inserción o actualización
+    RETURN NEW;
 END;
 $$;
 
@@ -156,3 +158,72 @@ CREATE OR REPLACE TRIGGER VERIFICAR_NOMBRE_SUCURSAL_TR
     BEFORE INSERT OR UPDATE ON PARQUEADERO.SUCURSAL
     FOR EACH ROW
     EXECUTE FUNCTION PARQUEADERO.VERIFICAR_NOMBRE_SUCURSAL_TR();
+
+
+-- Trigger que verifica que una sucursal solo tenga una tarifa activa.
+CREATE OR REPLACE FUNCTION PARQUEADERO.ACTUALIZACION_TARIFA_TR()
+RETURNS TRIGGER
+LANGUAGE PLPGSQL
+AS $$
+DECLARE
+    K_TARIFA_MINUTO_L PARQUEADERO.TARIFA_MINUTO.K_TARIFA_MINUTO%TYPE;
+    FECHA_INICIO_TARIFA_L PARQUEADERO.TARIFA_MINUTO.FECHA_INICIO_TARIFA%TYPE;
+BEGIN
+    -- Verifica que la PK de la tarifa anterior exista
+    -- Si no existe, esta es la primera tarifa que una sucursal va a insertar
+    IF EXISTS (
+        SELECT 1
+        FROM PARQUEADERO.TARIFA_MINUTO
+        WHERE K_SUCURSAL = NEW.K_SUCURSAL
+    ) THEN 
+        -- Recupera la PK y la fecha de inicio de la última sucursal
+        SELECT K_TARIFA_MINUTO,
+            FECHA_INICIO_TARIFA INTO STRICT K_TARIFA_MINUTO_L,
+            FECHA_INICIO_TARIFA_L
+        FROM PARQUEADERO.TARIFA_MINUTO
+        WHERE K_SUCURSAL = NEW.K_SUCURSAL
+            AND ESTA_ACTIVA;
+
+        -- Si han habido cambios recientes en la tarifa
+        IF (SELECT CURRENT_TIMESTAMP AT TIME ZONE 'America/Bogota') - FECHA_INICIO_TARIFA_L <= '0 YEARS 0 MONTHS 0 DAYS 0 HOURS 10 MINUTES'::INTERVAL THEN
+            -- Actualiza la tarifa actual sin crear una nueva
+            UPDATE PARQUEADERO.TARIFA_MINUTO
+            SET VALOR_MINUTO_SUV = NEW.VALOR_MINUTO_SUV,
+                VALOR_MINUTO_AUTO = NEW.VALOR_MINUTO_AUTO,
+                VALOR_MINUTO_MOTO = NEW.VALOR_MINUTO_MOTO,
+                ADICION_PARQ_CUBIERTO = NEW.ADICION_PARQ_CUBIERTO,
+                VALOR_MULTA_CANCELACION = NEW.VALOR_MULTA_CANCELACION
+            WHERE K_TARIFA_MINUTO = K_TARIFA_MINUTO_L;
+            
+            -- Aborta la inserción ya que no es necesaria
+            RETURN NULL;
+        ELSE
+            -- Actualiza la tarifa antigua, la deja inactiva y crea una nueva tarifa
+            UPDATE PARQUEADERO.TARIFA_MINUTO 
+            SET FECHA_FIN_TARIFA = (SELECT CURRENT_TIMESTAMP AT TIME ZONE 'America/Bogota'),
+                ESTA_ACTIVA = FALSE
+            WHERE K_TARIFA_MINUTO = K_TARIFA_MINUTO_L;
+        END IF;
+    END IF;
+    
+    -- Continúa con el INSERT
+    RETURN NEW;
+EXCEPTION 
+    -- Excepciones
+    WHEN TOO_MANY_ROWS THEN
+        ROLLBACK;
+        RAISE EXCEPTION 'Inconsistencias en la BD: Hay más de una tarifa activa para la sucursal de PK %, %/%',NEW.K_SUCURSAL, SQLSTATE, SQLERRM;
+    WHEN OTHERS THEN
+        ROLLBACK;
+        RAISE EXCEPTION 'MODIFICAR_TARIFA_SUCURSAL_PR ha ocurrido un error: %/%', SQLSTATE, SQLERRM;
+END;
+$$;
+
+COMMENT ON FUNCTION PARQUEADERO.ACTUALIZACION_TARIFA_TR IS E'Trigger que verifica que una sucursal solo tenga una tarifa activa.';
+
+ALTER FUNCTION PARQUEADERO.ACTUALIZACION_TARIFA_TR() OWNER TO PARKUD_DB_ADMIN ;
+
+CREATE OR REPLACE TRIGGER ACTUALIZACION_TARIFA_TR
+    BEFORE INSERT ON PARQUEADERO.TARIFA_MINUTO
+    FOR EACH ROW
+    EXECUTE FUNCTION PARQUEADERO.ACTUALIZACION_TARIFA_TR();
