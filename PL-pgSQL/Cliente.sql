b8289d121d171328b701ee77057997657be22f0a
Christian <chcarov@correo.udistrital.edu.co>
2023-06-03 15:16:14 -0500

    Función para mostrar las reservas hechas por un cliente implementada - Mejoras en el control de concurrencia.

--- PL-pgSQL/Cliente.sql
+++ PL-pgSQL/Cliente.sql
@@ -6,7 +6,7 @@ Los clientes registrados deben poder ver la información que tienen registrada e
 CREATE OR REPLACE FUNCTION PARQUEADERO.MOSTRAR_VEHICULOS_CLIENTE_FU()
 RETURNS JSON
 LANGUAGE PLPGSQL
-PARALLEL UNSAFE
+PARALLEL RESTRICTED
 AS $$
 DECLARE 
     -- Declaración de variables locales
@@ -18,11 +18,11 @@ DECLARE
     MENSAJE_ERROR_L TEXT;
 BEGIN
     -- Habilita la concurrencia de las tablas pero solo permite que puedan ser proyectadas
-    LOCK TABLE PARQUEADERO.MARCA_VEHICULO IN SHARE MODE;
+    LOCK TABLE PARQUEADERO.MARCA_VEHICULO IN ACCESS SHARE MODE;
 
-    -- Habilita la concurrencia de las tablas en distintas filas pero solo permite que puedan ser proyectadas
-    LOCK TABLE PARQUEADERO.CLIENTE IN ROW SHARE MODE;
-    LOCK TABLE PARQUEADERO.VEHICULO IN ROW SHARE MODE;
+    -- Habilita la concurrencia de las tablas en distintas filas pero solo permite que puedan ser proyectadas en una misma sesión
+    LOCK TABLE PARQUEADERO.CLIENTE IN SHARE UPDATE EXCLUSIVE MODE;
+    LOCK TABLE PARQUEADERO.VEHICULO IN SHARE UPDATE EXCLUSIVE MODE;
 
     -- Recupera la clave primaria del cliente conectado a la BD
     K_CLIENTE_L := PARQUEADERO.RECUPERAR_LLAVE_CLIENTE_FU();
@@ -40,7 +40,6 @@ BEGIN
             INNER JOIN PARQUEADERO.VEHICULO V ON C.K_CLIENTE = V.K_CLIENTE
             INNER JOIN PARQUEADERO.MARCA_VEHICULO MV ON V.K_MARCA_VEHICULO = MV.K_MARCA_VEHICULO
         WHERE C.K_CLIENTE = K_CLIENTE_L
-        FOR UPDATE
     ) T;
 
     -- Devuelve un JSON con la información de la consulta
@@ -58,4 +57,76 @@ $$;
 
 COMMENT ON FUNCTION PARQUEADERO.MOSTRAR_VEHICULOS_CLIENTE_FU IS E'Función para mostrar los vehículos que un cliente tiene registrados.';
 
-ALTER FUNCTION PARQUEADERO.MOSTRAR_VEHICULOS_CLIENTE_FU() OWNER TO PARKUD_DB_ADMIN;
+ALTER FUNCTION PARQUEADERO.MOSTRAR_VEHICULOS_CLIENTE_FU OWNER TO PARKUD_DB_ADMIN;
+
+
+-- El cliente requiere ver un registro de las reservas que ha hecho.
+-- La función devuelve todas las reservas hechas por un usuario.
+CREATE OR REPLACE FUNCTION PARQUEADERO.MOSTRAR_RESERVAS_CLIENTE_FU()
+RETURNS JSON
+LANGUAGE PLPGSQL
+PARALLEL RESTRICTED
+AS $$
+DECLARE
+    -- Declaración de variables locales
+    K_CLIENTE_L PARQUEADERO.CLIENTE.K_CLIENTE%TYPE;
+    RESULTADO_L JSON;
+    -- Códigos de error
+    CODIGO_ERROR_L TEXT;
+    RESUMEN_ERROR_L TEXT;
+    MENSAJE_ERROR_L TEXT;
+BEGIN
+    -- Habilita la concurrencia de las tablas pero solo permite que puedan ser proyectadas
+    LOCK PARQUEADERO.PAIS IN ACCESS SHARE MODE;
+    LOCK PARQUEADERO.CIUDAD IN ACCESS SHARE MODE;
+    LOCK PARQUEADERO.DIRECCION IN ACCESS SHARE MODE;
+    LOCK PARQUEADERO.SUCURSAL IN ACCESS SHARE MODE;
+    LOCK PARQUEADERO.TARIFA_MINUTO IN ACCESS SHARE MODE;
+    LOCK PARQUEADERO.HORARIO_SUCURSAL IN ACCESS SHARE MODE;
+    LOCK PARQUEADERO.SLOT_PARQUEADERO IN ACCESS SHARE MODE;
+    LOCK PARQUEADERO.DIA_SEMANA IN ACCESS SHARE MODE;
+    LOCK PARQUEADERO.RESERVA IN ACCESS SHARE MODE;
+
+    -- Recupera la clave primaria del cliente conectado a la BD
+    K_CLIENTE_L := PARQUEADERO.RECUPERAR_LLAVE_CLIENTE_FU();
+
+    -- Inserta en un JSON el resultado de la consulta
+    SELECT JSON_AGG(ROW_TO_JSON(T)) INTO RESULTADO_L 
+    FROM(
+        -- Consulta que devuelve la información básica de una reserva
+        SELECT C.NOMBRE_CIUDAD "Ciudad",
+            S.NOMBRE_SUCURSAL "Nombre sucursal",
+            D.NOMBRE_DIRECCION "Dirección",
+            SP.K_SLOT_PARQUEADERO "Slot",
+            R.FECHA_INICIO_RESERVA "Fecha reserva",
+            R.PLACA_VEHICULO "Placa",
+            R.VALOR_RESERVA "Total reserva"
+        FROM PARQUEADERO.PAIS P 
+            INNER JOIN PARQUEADERO.DEPARTAMENTO DP ON P.K_PAIS = DP.K_PAIS
+            INNER JOIN PARQUEADERO.CIUDAD C ON DP.K_DEPARTAMENTO = C.K_DEPARTAMENTO
+            INNER JOIN PARQUEADERO.DIRECCION D ON C.K_CIUDAD = D.K_CIUDAD
+            INNER JOIN PARQUEADERO.SUCURSAL S ON D.K_DIRECCION = S.K_DIRECCION
+            INNER JOIN PARQUEADERO.SLOT_PARQUEADERO SP ON S.K_SUCURSAL = SP.K_SUCURSAL
+            INNER JOIN PARQUEADERO.RESERVA R ON SP.K_SUCURSAL = R.K_SUCURSAL
+                AND SP.K_SLOT_PARQUEADERO = R.K_SLOT_PARQUEADERO
+                AND S.K_SUCURSAL = R.K_SUCURSAL
+            INNER JOIN PARQUEADERO.CLIENTE CL ON R.K_CLIENTE = CL.K_CLIENTE
+        WHERE CL.K_CLIENTE = K_CLIENTE_L
+    ) T;
+
+    -- Devuelve un JSON con la información de la consulta
+    RETURN RESULTADO_L;
+EXCEPTION
+    -- Excepciones
+    WHEN OTHERS THEN
+        GET STACKED DIAGNOSTICS 
+            CODIGO_ERROR_L := RETURNED_SQLSTATE,
+            RESUMEN_ERROR_L := MESSAGE_TEXT,
+            MENSAJE_ERROR_L := PG_EXCEPTION_CONTEXT;
+        RAISE EXCEPTION 'Código de error: % / Resumen del error: % / Mensaje de error: %', CODIGO_ERROR_L, RESUMEN_ERROR_L, MENSAJE_ERROR_L;
+END;
+$$;
+
+COMMENT ON FUNCTION PARQUEADERO.MOSTRAR_RESERVAS_CLIENTE_FU IS E'Función para mostrar las reservas que un cliente ha hecho.';
+
+ALTER FUNCTION PARQUEADERO.MOSTRAR_RESERVAS_CLIENTE_FU OWNER TO PARKUD_DB_ADMIN;
