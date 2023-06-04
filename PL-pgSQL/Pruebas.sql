1d2af1714fc4314693275c9aac8f215764bf4040
Christian <chcarov@correo.udistrital.edu.co>
2023-06-03 23:23:28 -0500

    Pruebas de creación de admins y súper admins.

--- PL-pgSQL/Pruebas.sql
+++ PL-pgSQL/Pruebas.sql
@@ -15,7 +15,7 @@ CALL PARQUEADERO.PRIMER_CAMBIO_CLAVE_PR('sdfrancoc@udistrital.edu.co', '1234');
 Pruebas: Adición de vehículos: */
 CALL PARQUEADERO.AGREGAR_VEHICULO_PR('Automóvil', 'CFD256', 'Christian', NULL, 'Caro', 'Vargas', 'Audi', 'Negro');
 CALL PARQUEADERO.AGREGAR_VEHICULO_PR('SUV', 'BWD456', 'Christian', NULL, 'Caro', 'Vargas', 'Skoda', 'Negro');
-CALL PARQUEADERO.AGREGAR_VEHICULO_PR('Automóvil', 'FDS123', 'Christian', NULL, 'Caro', 'Vargas', 'BMW', 'Blanco');
+CALL PARQUEADERO.AGREGAR_VEHICULO_PR('Moto', 'FDS123', 'Christian', NULL, 'Caro', 'Vargas', 'BMW', 'Blanco');
 CALL PARQUEADERO.AGREGAR_VEHICULO_PR('SUV', 'HTB236', 'Christian', NULL, 'Caro', 'Vargas', 'Fiat', 'Verde');
 CALL PARQUEADERO.AGREGAR_VEHICULO_PR('Automóvil', 'FSD263', 'Christian', NULL, 'Caro', 'Vargas', 'Honda', 'Azul');
 CALL PARQUEADERO.AGREGAR_VEHICULO_PR('SUV', 'FAS154', 'Samuel', 'David', 'Franco', 'Cuenca', 'Chevrolet', 'Plata');
@@ -39,7 +39,8 @@ SELECT PARQUEADERO.MOSTRAR_INFO_BASICA_SUCURSAL_FU('SUV', NULL, TRUE, NULL);
 SELECT PARQUEADERO.MOSTRAR_INFO_BASICA_SUCURSAL_FU('Automóvil', NULL, NULL, NULL);
 -- 3. Tercer paso
 SELECT PARQUEADERO.MOSTRAR_INFO_SUCURSAL_RESERVA_FU('Bogotá, D.C.', TRUE, 'SUV', 'Sucursal Chapinero');
-
+-- 4. CUarto paso (muestra los métodos de pago del cliente):
+SELECT PARQUEADERO.MOSTRAR_METODOS_PAGO_FU();
 --5 Quinto paso (hace la reserva):
 CALL PARQUEADERO.CREAR_RESERVA_PR('Automóvil', 'Audi - CFD256', TRUE, 'Bogotá, D.C.', 'Sucursal Kennedy', 'Carrera 77B # 58C - 41', '2023-05-29', '13:00', '0004', 'MasterCard', 'Christian', 'Caro', 0);
 CALL PARQUEADERO.CREAR_RESERVA_PR('SUV', 'BMW - FDS123', TRUE, 'Bogotá, D.C.', 'Sucursal Chapinero', 'Carrera 7B # 45A - 23', '2023-06-01', '08:00', '0004', 'MasterCard', 'Christian', 'Caro', 0);
@@ -64,3 +65,14 @@ CALL PARQUEADERO.MODIFICAR_TARIFA_SUCURSAL_PR('Sucursal Chapinero', 'Bogotá, D.
 /* ---------------------------------------------------------------------------------------------------------------------
 Pruebas: Modificar el horario de una sucursal: */
 CALL PARQUEADERO.MODIFICAR_HORARIO_SUCURSAL_PR('Sucursal Chapinero', 'Bogotá, D.C.', 'Semicubierta', 'Carrera 7B # 45A - 23', 'Viernes', '06:00', '23:00', FALSE, FALSE);
+
+/* ---------------------------------------------------------------------------------------------------------------------
+Pruebas: Súper administradores creados: */
+CREATE ROLE "mafeijoog@udistrital.edu.co" WITH INHERIT LOGIN PASSWORD 'MiguelFeijoo1234' IN ROLE SUPER_ADMIN_ROLE ;
+CREATE ROLE "hhramireza@udistrital.edu.co" WITH INHERIT LOGIN PASSWORD 'HelioRamirez1234' IN ROLE SUPER_ADMIN_ROLE ;
+
+
+/* ---------------------------------------------------------------------------------------------------------------------
+Pruebas: Administradores creados: */
+CREATE ROLE "ltramirezr@udistrital.edu.co" WITH INHERIT LOGIN PASSWORD '20182020098' IN ROLE ADMIN_ROLE;
+CREATE ROLE "adbeltranr@udistrital.edu.co" WITH INHERIT LOGIN PASSWORD '20182020090' IN ROLE ADMIN_ROLE;
